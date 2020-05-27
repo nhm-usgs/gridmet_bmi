ef0c2b6cf43061db8d5372f6fc6e6094bcf13719
Richard McDonald <rmcd@usgs.gov>
2020-05-27 12:07:46 -0600

    ore linting

--- gridmet_bmi/__init__.py
+++ gridmet_bmi/__init__.py
@@ -3,7 +3,7 @@ from .bmi import BmiGridmet
 from .gridmet import Gridmet
 from .helpers import np_get_wval, getaverage
 
-__all__ = ["BmiGridmet", "Gridmet"]
+__all__ = ["BmiGridmet", "Gridmet", "np_get_wval", "getaverage"]
 
 __version__ = get_versions()["version"]
 del get_versions
